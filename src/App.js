832ac853e56105a1d8d0456144707eae31d0aee4
Hayri Habip <hayrihabip@hotmail.com>
2024-03-24 22:59:49 +0300

    * Adding very first stage of "react-day-picker" package.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import { DayPicker } from 'react-day-picker';
+import { format } from 'date-fns';
+
+import 'react-day-picker/dist/style.css';
 import './App.css';
 
 function App() {
+  const [selected, setSelected] = useState(null);
+
+  let footer = <p>Please pick a day.</p>;
+  if (selected) {
+    footer = <p>You picked {format(selected, 'PP')}.</p>;
+  }
+
+  const bookedStyle = { border: '2px solid currentColor' };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DayPicker
+      // selected={new Date()}
+      onDayClick={(day) => console.log(day)}
+      showOutsideDays 
+      // fixedWeeks
+      // numberOfMonths={2}
+      // pagedNavigation
+      captionLayout="dropdown"
+      fromYear={2015}
+      toYear={2025}
+      weekStartsOn={1}
+      // fixedWeeks={false}
+      // showWeekNumber
+      // locale="tr"
+      // month={new Date()}
+      modifiers={{
+        // selected: [
+        //   {
+        //     dayOfWeek: [1],
+        //   },
+        // ],
+        disabled: [
+          {
+            dayOfWeek: [2],
+          },
+        ],
+        // outside: [
+        //   {
+        //     dayOfWeek: [3],
+        //   },
+        // ],
+        booked: [
+          {
+            dayOfWeek: [3],
+          }
+        ]
+      }}
+      modifiersStyles={{
+        booked: bookedStyle,
+      }}
+    />
   );
 }
 
